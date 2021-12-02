0aef17afe88b91edeb1794173ae0d1a5b1c609c6
johnny-rob <jpinney1@gmail.com>
2021-12-02 08:51:42 -0500

    Implemented shared navigationwith minimal styling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+
+  ## The next two def sections work together to create the nav items
+    def nav_items
+      [
+        {
+          url: root_path,
+          title: 'Home'
+        },
+        {
+          url: blogs_path,
+          title: 'Blog'
+        },
+      ]
+    end
+
+    def nav_helper style, tag_type
+      nav_links = ''
+      nav_items.each do |item|
+        nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+      end
+
+      nav_links.html_safe
+    end
+
+    def active? path
+      "active" if current_page? path
+    end
+
 end
